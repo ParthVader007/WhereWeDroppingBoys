bb61e4667a9e1ba4c7ab58a73b3df599160121d1
Kevin Li <kevin@Kevins-MacBook-Pro.local>
2019-02-16 13:45:16 -0600

    playing around with main file

--- classes.py
+++ classes.py
@@ -9,27 +9,28 @@ HP = "health"
 unit_dict = {HP: 0, ATTACK: 0, DEFENSE: 0}
 
 class Unit(object):
-	'''
-	Basic Unit Object
-	'''
+    '''
+    Basic Unit Object
+    '''
 
-	def __init__(self, unit_name, unit_stats_dict):
-	self.unit_stats = unit_dict
-	self.unit_name = unit_name
+    def __init__(self, unit_name, unit_stats_dict):
+        self.unit_stats = unit_dict
+        self.unit_name = unit_name
 
 
-	def take_damage(enemy_attack):
-	'''
-	Reduces a units hp dependent on the enemy's attack
-	'''
+    def take_damage(enemy_attack):
+        '''
+        Reduces a units hp dependent on the enemy's attack
+        '''
+        damage = enemy_attack - unit_dict[DEFENSE];
+        if damage < 0:
+            damage = 0
+        unit_stats[HP] -= damage
+        return None
 
-	damage = enemy_attack - unit_dict[DEFENSE];
-	if damage < 0:
-		damage = 0
-	unit_stats[HP] -= damage
-	return None
+    
 
-	
+    
 
 
 
---
+++ game.py
@@ -0,0 +1,17 @@
+import random
+from classes.py import *
+
+ATTACK = "attack"
+DEFENSE = "defense"
+HP = "health"
+
+HERO_NAME = "Hero"
+HERO_DICT = {HP: 50, ATTACK = 10, DEFENSE = 5}
+
+hero = Unit(HERO_NAME, HERO_DICT)
+
+while hero.unit_stats[HP] > 0
+{
+	#whatever the fuck
+
+}
