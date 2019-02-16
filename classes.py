468569aea6490cf9d7af33e29a13cf74ca22031c
Kevin Li <kevin@Kevins-MacBook-Pro.local>
2019-02-16 13:32:36 -0600

    updated classes

--- classes.py
+++ classes.py
@@ -1,8 +1,12 @@
 import random
 
+ATTACK = "attack"
+DEFENSE = "defense"
+HP = "health"
 
 
-unit_stats_dict = {"health": 0, "attack": 0, "defense": 0}
+
+unit_dict = {HP: 0, ATTACK: 0, DEFENSE: 0}
 
 class Unit(object):
 	'''
@@ -10,8 +14,22 @@ class Unit(object):
 	'''
 
 	def __init__(self, unit_name, unit_stats_dict):
-	self.unit_stats = unit_stats_dict
+	self.unit_stats = unit_dict
 	self.unit_name = unit_name
+
+
+	def take_damage(enemy_attack):
+	'''
+	Reduces a units hp dependent on the enemy's attack
+	'''
+
+	damage = enemy_attack - unit_dict[DEFENSE];
+	if damage < 0:
+		damage = 0
+	unit_stats[HP] -= damage
+	return None
+
+	
 
 
 
